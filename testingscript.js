3af1e8a775942820d201f6d80f601e51c0faa6ba
evan <coolmineralsandfossils@Gmail.com>
2025-03-24 22:23:54 -0500

    Update testingscript.js

--- testingscript.js
+++ testingscript.js
@@ -1,34 +1,34 @@
 (() => {
   "use strict";
-  function t(t, i = !1) {
-    let e = window.open(i ? "about:blank" : "https://classroom.google.com");
-    e.document.write(
-      '\n        <body style="margin:0;">\n            <title>Home</title>\n\n            <embed src="https://hexbois.com" style="width:100vw;height:100vh;">\n\n            <script>\n            window.addEventListener("beforeunload", (ev)=>{\n                ev.returnValue = "a"\n                ev.preventDefault()\n                return "aa"\n            })\n            </script>\n        </body>\n    '
+  function open(open, submission = !1) {
+    let enteredUrl = window.open(submission ? "about:blank" : "https://classroom.google.com");
+    enteredUrl.document.write(
+      '\n        <body style="margin:0;">\n            <title>Home</title>\n\n            <embed src="https://hexbois.com" style="width:100vw;height:100vh;">\n\n            <script>\n            window.addEventListener("beforeunload", (ev)=>{\n                ev.returnValue = "chromeCheck"\n                ev.preventDefault()\n                return "aa"\n            })\n            </script>\n        </body>\n    '
     );
-    let r = e.document.createElement("link");
+    let r = enteredUrl.document.createElement("link");
     (r.rel = "icon"),
       (r.href =
         "https://www.gstatic.com/classroom/ic_product_classroom_144.png"),
-      e.document.head.appendChild(r),
-      (e.document.querySelector("embed").src = t);
+      enteredUrl.document.head.appendChild(r),
+      (enteredUrl.document.querySelector("embed").src = open);
   }
-  let i, e;
-  let n, g, a = !!window.chrome;
+  let submission, enteredUrl;
+  let n, chromeCheck = !!window.chrome;
 
   
-    (document.body.innerHTML = a
-      ? '<body>\r\n    <head>\r\n        <meta charset="UTF-8">\r\n        <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n        <title>Launcher</title>\r\n    \r\n    </head>\r\n    <style id="style"></style>\r\n\r\n    <form id="enter-url-form"> \r\n        <input id="enter-url" type="text" placeholder="Custom URL (eg. https://hexbois.com)">\r\n    </form>\r\n   <div id="games"></div>\r\n\r\n</body>'
+    (document.body.innerHTML = chromeCheck
+      ? '<body>\r\n    <head>\r\n        <meta charset="UTF-8">\r\n        <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n        <title>Launcher</title>\r\n    \r\n    </head>\r\n    <style id="style"></style>\r\n\r\n    <form id="enter-url-form"> \r\n        <input id="enter-url" type="text" placeholder="Enter URL...">\r\n    </form>\r\n   <div id="games"></div>\r\n\r\n</body>'
       : '<body>\r\n    <head>\r\n        <meta charset="UTF-8">\r\n        <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n        <title>Launcher Error</title>\r\n    \r\n    </head>\r\n    <style id="style"></style>\r\n\r\n    <p>This launcher is only functional on Chromium based browsers.</p>\r\n\r\n</body>'),
     (document.querySelector("#style").innerHTML = "/*stylehere*/"),
     
-    a &&
-      ((i = document.querySelector("#enter-url-form")),
-      (e = document.querySelector("#enter-url")),
-      i.addEventListener("submit", function (i) {
-        i.preventDefault(), t(e.value);
+    chromeCheck &&
+      ((submission = document.querySelector("#enter-url-form")),
+      (enteredUrl = document.querySelector("#enter-url")),
+      submission.addEventListener("submit", function (submission) {
+        submission.preventDefault(), open(enteredUrl.value);
       }),
       (n = document.querySelector("#games")),
-      r.M.forEach((i) => {
-        n.appendChild(e);
+      r.M.forEach((submission) => {
+        n.appendChild(enteredUrl);
       }));
 })();
