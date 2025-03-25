2e27cb5394f6087c45f92f83569726c0e9e73b5c
evan <coolmineralsandfossils@Gmail.com>
2025-03-24 22:04:52 -0500

    Update testingscript.js

--- testingscript.js
+++ testingscript.js
@@ -1,60 +1,34 @@
 (() => {
-    "use strict";
+  "use strict";
+  function t(t, i = !1) {
+    let e = window.open(i ? "about:blank" : "https://classroom.google.com");
+    e.document.write(
+      '\n        <body style="margin:0;">\n            <title>Home</title>\n\n            <embed src="https://hexbois.com" style="width:100vw;height:100vh;">\n\n            <script>\n            window.addEventListener("beforeunload", (ev)=>{\n                ev.returnValue = "a"\n                ev.preventDefault()\n                return "aa"\n            })\n            </script>\n        </body>\n    '
+    );
+    let r = e.document.createElement("link");
+    (r.rel = "icon"),
+      (r.href =
+        "https://www.gstatic.com/classroom/ic_product_classroom_144.png"),
+      e.document.head.appendChild(r),
+      (e.document.querySelector("embed").src = t);
+  }
+  let i, e;
+  let n, g, a = !!window.chrome;
 
-    function openURL(url, isBlank = false) {
-        let newWindow = window.open(isBlank ? "about:blank" : "https://classroom.google.com");
-        newWindow.document.write(`
-            <body style="margin:0;">
-                <title>Home</title>
-            </body>
-        `);
-        let iconLink = newWindow.document.createElement("link");
-        iconLink.rel = "icon";
-        iconLink.href = "https://www.gstatic.com/classroom/ic_product_classroom_144.png";
-        newWindow.document.head.appendChild(iconLink);
-        newWindow.document.querySelector("embed").src = url;
-    }
-
-    let htmlCode = !!window.chrome;
-
-    document.body.innerHTML = htmlCode ? `
-        <body>
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Launcher</title>
-            </head>
-            <style id="style"></style>
-            <form id="enter-url-form">
-                <input id="enter-url" type="text" placeholder="Enter URL...">
-            </form>
-        </body>
-    ` : `
-        <body>
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Error</title>
-            </head>
-            <style id="style"></style>
-            <p>This proxy is only functional on Chrome.</p>
-        </body>
-    `;
-
-    document.querySelector("#style").innerHTML = `
-/*styleshere*/
-    `;
-
-    if (htmlCode) {
-        const form = document.querySelector("#enter-url-form");
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const enteredUrl = document.querySelector("#enter-url").value;
-            if (enteredUrl && enteredUrl.length && enteredUrl.startsWith("http") && enteredUrl.includes(".")) {
-                window.location.replace(enteredUrl);
-            } else {
-                alert("Invalid URL.");
-            }
-        });
-    }
+  
+    (document.body.innerHTML = a
+      ? '<body>\r\n    <head>\r\n        <meta charset="UTF-8">\r\n        <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n        <title>Launcher</title>\r\n    \r\n    </head>\r\n    <style id="style"></style>\r\n\r\n    <form id="enter-url-form"> \r\n        <input id="enter-url" type="text" placeholder="Custom URL (eg. https://hexbois.com)">\r\n    </form>\r\n   <div id="games"></div>\r\n\r\n</body>'
+      : '<body>\r\n    <head>\r\n        <meta charset="UTF-8">\r\n        <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n        <title>Launcher Error</title>\r\n    \r\n    </head>\r\n    <style id="style"></style>\r\n\r\n    <p>This launcher is only functional on Chromium based browsers.</p>\r\n\r\n</body>'),
+    (document.querySelector("#style").innerHTML = "/*stylehere*/"),
+    
+    a &&
+      ((i = document.querySelector("#enter-url-form")),
+      (e = document.querySelector("#enter-url")),
+      i.addEventListener("submit", function (i) {
+        i.preventDefault(), t(e.value);
+      }),
+      (n = document.querySelector("#games")),
+      r.M.forEach((i) => {
+        n.appendChild(e);
+      }));
 })();
